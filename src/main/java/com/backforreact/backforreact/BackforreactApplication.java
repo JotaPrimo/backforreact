19a020fdf528f3b742bd56c8b57f6dfa572b9567
Jota Santos <jailsonsantosdpge@outlook.com>
2025-09-09 18:40:23 -0400

    projeto iniciado

---
+++ src/main/java/com/backforreact/backforreact/BackforreactApplication.java
@@ -0,0 +1,13 @@
+package com.backforreact.backforreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackforreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackforreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backforreact/backforreact/BackforreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backforreact.backforreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackforreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
